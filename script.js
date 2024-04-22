bd1d88db01ef6f04777ec5e259cd3349a665f88d
Dark12Phantom <144307615+Dark12Phantom@users.noreply.github.com>
2024-04-22 11:59:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+var state = document.querySelector('#state');
+var input = document.querySelector('#input');
 
+var btn1 = document.querySelector('.btn1');
+var btn3 = document.querySelector('.btn3');
+var btn4 = document.querySelector('.btn4');
+var btn2 = document.querySelector('.btn2');
+var btn5 = document.querySelector('.btn5');
+var btn6 = document.querySelector('.btn6');
+var btn7 = document.querySelector('.btn7');
+var btn8 = document.querySelector('.btn8');
+var btn9 = document.querySelector('.btn9');
+var btn10 = document.querySelector('.btn10');
+var btn11 = document.querySelector('.btn11');
+var btn = document.querySelector('.btn');
+
+btn1.addEventListener('click',() => {
+    state.placeholder = 'q0';
+});
+
+btn2.addEventListener('click',() => {
+    state.placeholder = 'q1';
+});
+
+btn3.addEventListener('click',() => {
+    state.placeholder = 'q2';
+});
