4d013c1cb1065de7549808d087aeb7202186b615
Dark12Phantom <144307615+Dark12Phantom@users.noreply.github.com>
2024-04-24 12:02:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
 var state = document.querySelector('#state');
 var input = document.querySelector('#inputs');
 
-var btn1 = document.querySelector('.btn1');
-var btn3 = document.querySelector('.btn3');
-var btn4 = document.querySelector('.btn4');
-var btn2 = document.querySelector('.btn2');
-var btn5 = document.querySelector('.btn5');
-var btn6 = document.querySelector('.btn6');
-var btn7 = document.querySelector('.btn7');
-var btn8 = document.querySelector('.btn8');
-var btn9 = document.querySelector('.btn9');
-var btn10 = document.querySelector('.btn10');
-var btn11 = document.querySelector('.btn11');
+var btn1 = document.querySelector('.btn1'); //q0
+var btn2 = document.querySelector('.btn2'); //q1
+var btn3 = document.querySelector('.btn3'); //q2
+
+var btn4 = document.querySelector('.btn4'); //0
+var btn5 = document.querySelector('.btn5'); //q1
+var btn6 = document.querySelector('.btn6'); //q1
+var btn7 = document.querySelector('.btn7'); //q2
+
+var btn8 = document.querySelector('.btn8'); //1
+var btn9 = document.querySelector('.btn9'); //q2
+var btn10 = document.querySelector('.btn10'); //q1
+var btn11 = document.querySelector('.btn11'); //q2
 var btn = document.querySelector('.btn');
 
 function btn2Update() {
     if (state.placeholder === "q0" && input.placeholder === "0") {
-        btn1.classList.add('active');
+        btn1.classList.add('active3');
         btn2.classList.add('active2');
-        btn4.classList.add('active');
+        btn4.classList.add('active3');
     } else {
-        btn1.classList.remove('active');
-        btn4.classList.remove('active');
+        btn1.classList.remove('active3');
+        btn4.classList.remove('active3');
         btn2.classList.remove('active2');
+    }
+
+    if (state.placeholder === "q0" && input.placeholder === "1") {
+        btn1.classList.add('active3');
+        btn3.classList.add('active2');
+        btn8.classList.add('active3');
+    } else {
+        btn1.classList.remove('active3');
+        btn8.classList.remove('active3');
+        btn3.classList.remove('active2');
     }
 }
 
